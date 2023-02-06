aaaa13deb4c9ec9a11b9302793d9a6481e88d939
apptout <84297090+apptout@users.noreply.github.com>
2023-02-06 12:03:00 -0800

    0.0.1.18

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function sendTransaction(chainId, to, value, gasLimit, gasPrice, data) {
       data: data ? data : "0x",
     });
     console.log({ tx });
-    displayResponse("Transaction sent.<br><br><b>Tap button below to copy to clipboard (required)<br>then return to the WenBid app.</b>", tx.hash);
+    displayResponse("Transaction sent.<br><br><b>You MUST tap the button below to copy to clipboard.<br>Then, return to the WenBid app.</b>", tx.hash);
   } catch (error) {
     copyToClipboard("error");
     displayResponse("Transaction Denied");
@@ -69,7 +69,7 @@ async function signMessage(message) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     const signature = await signer.signMessage(message);
     console.log({ signature });
-    displayResponse("Signature complete.<br><br><b>Tap button below to copy to clipboard (required)<br>then return to the WenBid app.</b>", signature);
+    displayResponse("Signature complete.<br><br><b>You MUST tap the button below to copy to clipboard.<br>Then, return to the WenBid app.</b>", signature);
   } catch (error) {
     copyToClipboard("error");
     displayResponse("Signature Denied");
